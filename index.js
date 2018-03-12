257bd00177d35ffe92c819a0f64c1a791b19b555
Zahran Kabir <zahran@Zahrans-MacBook-Pro.local>
2018-03-12 14:08:11 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+
+function upperCaseCustomerName(){
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+  bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(){
+  bestCustomer = 'maybe bob'
+}
+
+
+const leastFavoriteCustomer = 'steve'
+
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = 'john'
+}
